8076fef685a3777aa05062d162110032e62bdec1
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-27 16:13:14 -0300

    Create BookRepository

---
+++ api/src/application/interfaces/BookRepositoryInterface.ts
@@ -0,0 +1,11 @@
+import BookObject from "../../domain/interfaces/BookObject"
+
+interface BookRepositoryInterface {
+    create(props: BookObject): Promise<void>
+    update(props: BookObject): Promise<void>
+    delete(id: string): Promise<void>
+    show(): Promise<BookObject[]>
+    view(isbn: string): Promise<BookObject | null>
+}
+
+export default BookRepositoryInterface
---
+++ api/src/infrastructure/repositories/BookRepository.ts
@@ -0,0 +1,36 @@
+import { DataSource, Repository } from "typeorm";
+import BookRepositoryInterface from "../../application/interfaces/BookRepositoryInterface";
+import BookObject from "../../domain/interfaces/BookObject";
+import BookModel from "../models/BookModel";
+
+class BookRepository implements BookRepositoryInterface {
+    #connection: Repository<BookModel>
+
+    constructor(appDataSource: DataSource) {
+        this.#connection = appDataSource.getRepository(BookModel)
+    }
+
+    async create(props: BookObject): Promise<void> {
+        const book = this.#connection.create(props)
+        await this.#connection.save(book)
+    }
+
+    async update(props: BookObject): Promise<void> {
+        await this.#connection.update(props.isbn, props)
+    }
+
+    async delete(id: string): Promise<void> {
+        await this.#connection.delete(id)
+    }
+
+    async show(): Promise<BookObject[]> {
+        return await this.#connection.find()
+    }
+
+    async view(isbn: string): Promise<BookObject | null> {
+        return await this.#connection.findOneBy({isbn})
+    }
+
+}
+
+export default BookRepository
