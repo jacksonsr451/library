db30a22305b1e77541d85e59f2da1a6edabfe872
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-27 14:46:38 -0300

    Prepare data-source

---
+++ api/src/data-source.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+import path from 'path'
+import 'reflect-metadata'
+
+import { DataSource } from 'typeorm'
+
+const appDataSource = new DataSource(
+    process.env.PRODUCTION === 'true' ? 
+    {
+        type: 'mongodb',
+        url: process.env.DB_URL,
+        synchronize: true,
+        entities: [path.resolve(`./infrastructure/models/*.{ts,js}`)],
+        migrations: [path.resolve('./migrations/*.{ts,js}')]
+    } : {
+        type: 'better-sqlite3',
+        database: 'database.sqlite',
+        synchronize: true,
+        entities: [path.resolve('./infrastructure/models/*.{ts,js}')],
+        migrations: [path.resolve('./migrations/*.{ts,js}')]
+    }
+)
+
+export default appDataSource
