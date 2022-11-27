febb50ee9780d10f9f557ebf56eea2567a8beb8d
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-27 15:35:45 -0300

    Refactoeing data-source

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -4,20 +4,27 @@ import 'reflect-metadata'
 
 import { DataSource } from 'typeorm'
 
+// eslint-disable-next-line no-eval
+// const value = eval(process.env.PRODUCTION)
+
+console.log(path.resolve('./src/infrastructure/models/*.{ts,js}'))
+console.log(path.resolve('./migrations/*.{ts,js}'))
+console.log(process.env.PRODUCTION)
+
 const appDataSource = new DataSource(
     process.env.PRODUCTION === 'true' ? 
     {
         type: 'mongodb',
         url: process.env.DB_URL,
         synchronize: true,
-        entities: [path.resolve(`./infrastructure/models/*.{ts,js}`)],
-        migrations: [path.resolve('../migrations/*.{ts,js}')]
+        entities: [path.resolve('./src/infrastructure/models/*.{ts,js}')],
+        migrations: [path.resolve('./migrations/*.{ts,js}')]
     } : {
         type: 'better-sqlite3',
         database: 'database.sqlite',
         synchronize: true,
-        entities: [path.resolve('./infrastructure/models/*.{ts,js}')],
-        migrations: [path.resolve('../migrations/*.{ts,js}')]
+        entities: [path.resolve('./src/infrastructure/models/*.{ts,js}')],
+        migrations: [path.resolve('./migrations/*.{ts,js}')]
     }
 )
 
