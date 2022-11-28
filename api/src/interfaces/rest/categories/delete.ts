7945847d333c0769e09e047b870a3ba1db995687
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-28 10:31:56 -0300

    Implementing delete category controller

--- api/src/interfaces/rest/categories/delete.ts
+++ api/src/interfaces/rest/categories/delete.ts
@@ -3,7 +3,14 @@ import CategoryService from "../../../application/usecases/CategoryServices"
 
 const remove = (service: CategoryService) => 
     async (request: Request, response: Response, next: NextFunction) => {
+        const { id } = request.params
 
+        try {
+            await service.delete(id)
+            return response.status(200).json({message: "Categoria deletada com sucesso!"})
+        } catch (error) {
+            return response.status(400).json({error})
+        }
 }
 
 export default remove
