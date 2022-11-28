04993272b324c21f5c1d49fd1769f18a95268e0d
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-28 11:01:53 -0300

    Implementing delete library controller

--- api/src/interfaces/rest/libraries/delete.ts
+++ api/src/interfaces/rest/libraries/delete.ts
@@ -3,7 +3,14 @@ import LibraryService from "../../../application/usecases/LibraryService"
 
 const remove = (service: LibraryService) => 
     async (request: Request, response: Response, next: NextFunction) => {
+        const { id } = request.params
 
+        try {
+            await service.delete(id)
+            return response.status(200).json({message: "Removido com sucesso!"})
+        } catch (error) {
+            return response.status(400).json({error})
+        }
 }
 
 export default remove
