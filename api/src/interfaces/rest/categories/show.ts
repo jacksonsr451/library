b60bab4506ecca5ef3d445805134cf6ff70c9420
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-28 10:19:32 -0300

    Implementing show categories controller

--- api/src/interfaces/rest/categories/show.ts
+++ api/src/interfaces/rest/categories/show.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express"
 import CategoryService from "../../../application/usecases/CategoryServices"
 
-const show = (service: CategoryService) => async (request: Request, response: Response) => {
-
+const show = (service: CategoryService) => 
+    async (request: Request, response: Response) => {
+        try {
+            const books = await service.show()
+            return response.status(200).json(books)
+        } catch (error) {
+            return response.status(400).json({error: error})
+        }
 }
 
 export default show
