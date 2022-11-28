801df47c5fa191500f98fa042f4f9f0dbb6c4011
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-28 09:53:08 -0300

    Implementing delete book controller

--- api/src/interfaces/rest/books/delete.ts
+++ api/src/interfaces/rest/books/delete.ts
@@ -3,7 +3,13 @@ import BookService from "../../../application/usecases/BookService"
 
 const remove = (service: BookService) => 
     async (request: Request, response: Response, next: NextFunction) => {
-
+        const { isbn } = request.params
+        try {
+            await service.delete(isbn)
+            return response.status(200).json({message: "Deletado com sucesso!"})
+        } catch (error) {
+            return response.status(400).json({error: error})
+        }
 }
 
 export default remove
