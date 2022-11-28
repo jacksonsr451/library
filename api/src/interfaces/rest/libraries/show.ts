32c464a64f395e6ed88e3691234c10be99134167
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-28 10:46:31 -0300

    Implementing show library controller

--- api/src/interfaces/rest/libraries/show.ts
+++ api/src/interfaces/rest/libraries/show.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express"
 import LibraryService from "../../../application/usecases/LibraryService"
 
-const show = (service: LibraryService) => async (request: Request, response: Response) => {
-
+const show = (service: LibraryService) => 
+    async (request: Request, response: Response) => {
+        try {
+            const libraries = await service.show()
+            return response.status(200).json(libraries)
+        } catch (error) {
+            return response.status(400).json({error})
+        }
 }
 
 export default show
