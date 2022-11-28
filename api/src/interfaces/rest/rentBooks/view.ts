65ce4a566fcb96115fb98583fb098c6852a1bd5f
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-28 11:42:39 -0300

    Implementing view rent-book controller

--- api/src/interfaces/rest/rentBooks/view.ts
+++ api/src/interfaces/rest/rentBooks/view.ts
@@ -3,7 +3,14 @@ import RentBookService from "../../../application/usecases/RentBookService"
 
 const view = (service: RentBookService) => 
     async (request: Request, response: Response, next: NextFunction) => {
+        const { id } = request.params
 
+        try {
+            const rentBook = await service.view(id)
+            return response.status(200).json(rentBook)
+        } catch (error) {
+            return response.status(400).json({error})
+        }
 }
 
 export default view
