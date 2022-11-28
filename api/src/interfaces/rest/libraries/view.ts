dc493ea050e27db4c64567e02460a75883dfa6fb
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-28 10:49:26 -0300

    Implementing view library controller

--- api/src/interfaces/rest/libraries/view.ts
+++ api/src/interfaces/rest/libraries/view.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express"
 import LibraryService from "../../../application/usecases/LibraryService"
 
-const view = (service: LibraryService) => async (request: Request, response: Response) => {
+const view = (service: LibraryService) => 
+    async (request: Request, response: Response) => {
+        const { id } = request.params
 
+        try {
+            const library = await service.view(id)
+            return response.status(200).json(library)
+        } catch (error) {
+            return response.status(400).json({error})
+        }
 }
 
 export default view
