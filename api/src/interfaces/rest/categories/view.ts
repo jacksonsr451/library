9319c17f993efb28c7fce97634ccee88482bdb6c
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-28 10:21:31 -0300

    Implementing view category controller

--- api/src/interfaces/rest/categories/view.ts
+++ api/src/interfaces/rest/categories/view.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express"
 import CategoryService from "../../../application/usecases/CategoryServices"
 
-const view = (service: CategoryService) => async (request: Request, response: Response) => {
-
+const view = (service: CategoryService) => 
+    async (request: Request, response: Response) => {
+        const { id } = request.params
+        try {
+            const category = await service.view(id)
+            return response.status(200).json(category)
+        } catch (error) {
+            return response.status(400).json({error: error})
+        }
 }
 
 export default view
