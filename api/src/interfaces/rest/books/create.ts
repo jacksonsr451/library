13d0888073470a833727f3e9281e76737d8a6a50
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-28 09:39:07 -0300

    Implementing create book controller

--- api/src/interfaces/rest/books/create.ts
+++ api/src/interfaces/rest/books/create.ts
@@ -4,20 +4,20 @@ import BookService from "../../../application/usecases/BookService"
 
 const create = (service: BookService) => 
     async (request: Request, response: Response, next: NextFunction) => {
-    const schema = z.object({
-        isbn: z.string(),
-        title: z.string(),
-        author: z.string(),
-        coAuthro: z.array(z.string()),
-        description: z.string(),
-    })
-    try {
-        const book = await schema.parseAsync(request.body)
-        await service.create(book)
-        return response.status(201).json({sucess: "Inserido com sucesso!"})
-    } catch (error) {
-        return response.status(400).json({error: error})
-    }
+        const schema = z.object({
+            isbn: z.string(),
+            title: z.string(),
+            author: z.string(),
+            coAuthro: z.array(z.string()),
+            description: z.string(),
+        })
+        try {
+            const book = await schema.parseAsync(request.body)
+            await service.create(book)
+            return response.status(201).json({sucess: "Inserido com sucesso!"})
+        } catch (error) {
+            return response.status(400).json({error: error})
+        }
 }
 
 export default create
