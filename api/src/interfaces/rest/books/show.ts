8693c72670cdf3ea22c1988dc168e0d62c2319b9
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-28 09:39:21 -0300

    Implementing show book controller

--- api/src/interfaces/rest/books/show.ts
+++ api/src/interfaces/rest/books/show.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express"
 import BookService from "../../../application/usecases/BookService"
 
-const show = (service: BookService) => async (request: Request, response: Response) => {
-
+const show = (service: BookService) => 
+    async (request: Request, response: Response) => {
+        try {
+            const books = await service.show()
+            return response.status(200).json(books)
+        } catch (error) {
+            return response.status(400).json(error)
+        }
 }
 
 export default show
