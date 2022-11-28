3df3262c1696134ef860e059e535a3819c1f5baa
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-11-28 09:39:35 -0300

    Implementing view book controller

--- api/src/interfaces/rest/books/view.ts
+++ api/src/interfaces/rest/books/view.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express"
 import BookService from "../../../application/usecases/BookService"
 
-const view = (service: BookService) => async (request: Request, response: Response) => {
-
+const view = (service: BookService) => 
+    async (request: Request, response: Response) => {
+        const { id } = request.params
+        try {
+            const book = await service.view(id)
+            return response.status(200).json(book)
+        } catch (error) {
+            return response.status(400).json(error)
+        }
 }
 
 export default view
